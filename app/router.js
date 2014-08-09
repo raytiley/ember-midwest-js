94df243cd660a47b25c4f95db0c8c69158115d37
raytiley <raytiley@gmail.com>
2014-08-09 08:34:48 -0400

    Defining Routes

--- app/router.js
+++ app/router.js
@@ -5,6 +5,30 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+	/**
+	  * Routes should be sued for verbs
+	**/
+  this.route('signin');
+  
+
+  /**
+    * Resources shoudl be used for nounes
+  **/
+  this.resource('dashboard');
+  this.resource('profile', {path: '/user/:id'});
+  this.resource('workouts', {path: '/workouts/:id'});
+
+  /**
+    * Nested Routes allow for nested uis
+    * The following will create the following routes:
+    *  activities.index
+    *  activities.detail
+    * 
+    * both will be rendered in the activites.hbs {{outlet}}
+  **/
+  this.resource('activities', function() {
+    this.route('detail', {path: ':id'});
+  });
 });
 
 export default Router;
